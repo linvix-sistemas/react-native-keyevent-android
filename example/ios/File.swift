cab676def9f949a03ecf4b212d3ddb364fe27b64
Cristiano Cruz <cristiano@linvix.com.br>
2022-08-19 15:30:24 -0300

    remoção example

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  Keyevent-androidExample
-//
-
-import Foundation
--- example/react-native.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  dependencies: {
-    '@linvix-sistemas/react-native-keyevent-android': {
-      root: path.join(__dirname, '..'),
-    },
-  },
-};
